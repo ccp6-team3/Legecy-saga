887bd26544f54af1eb37df5ef4bf34d663d15fa0
YJK-7 <2019yjk@gmail.com>
2022-10-22 16:26:42 +0900

    feat: added navigation and fetch button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,54 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './componets/Navbar.js'
+
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+
 
 function App() {
+  const [info, setInfo] = useState(null);
+  // const [num, setNum] = useState(0);
+
+  const fetch = () => {
+    return fetch('/test')
+    .then((data) => {
+      return data.json();
+    })
+    .then((dataJS) => {
+      setInfo(dataJS);
+    });
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Saga</p>
-      </header>
-    </div>
+    <>
+    <NavigationBar />
+    {/*<Container fluid variant='primary' key='primary'>
+      <Row>
+        {/* <Col>
+          <h1 id='title'>Saga</h1>
+        </Col> }
+
+        <Col>
+        </Col>
+      </Row>
+    </Container>*/}
+
+    <Button variant="outline-primary" onClick={fetch}>Fetch</Button>{' '}
+    {info}
+    {/* <button type="button" class="btn-success" onClick={fetch}>
+          Fetch info:
+    </button> */}
+    </>
   );
 }
 
