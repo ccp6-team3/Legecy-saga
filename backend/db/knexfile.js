00cd4c32a5b44a77ab5f4c2561b24f2918f022e1
Brian Walvoord <brian.walvoord@gmail.com>
2022-10-23 20:43:35 +0900

    fix: changed fetch function name and stringified the result for rendering

--- backend/db/knexfile.js
+++ backend/db/knexfile.js
@@ -1,9 +1,6 @@
-<<<<<<< Updated upstream
-require('dotenv').config({path: "./../../.env.local",});
-
-=======
 require('dotenv').config({path:__dirname+'/../../.env.local'})
->>>>>>> Stashed changes
+// require('dotenv').config({path: "./.env.local"})
+
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -22,15 +19,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-<<<<<<< Updated upstream
-        database: process.env.DB_NAME || "sagaproject",
-        user: process.env.DB_USER || "postgres",
-        password: process.env.DB_PASSWORD || "Osaka"
-=======
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
->>>>>>> Stashed changes
     },
     migrations: {
         directory: "./migrations"
