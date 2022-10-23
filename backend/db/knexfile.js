acf88533f402b86fb9b464c91f09c4f424f3b84c
Pol Texido <107403548+calss0t@users.noreply.github.com>
2022-10-23 20:26:17 +0900

    Added proxy and changed path to the env.local file

--- backend/db/knexfile.js
+++ backend/db/knexfile.js
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 require('dotenv').config({path: "./../../.env.local",});
 
+=======
+require('dotenv').config({path:__dirname+'/../../.env.local'})
+>>>>>>> Stashed changes
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -18,9 +22,15 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
+<<<<<<< Updated upstream
         database: process.env.DB_NAME || "sagaproject",
         user: process.env.DB_USER || "postgres",
         password: process.env.DB_PASSWORD || "Osaka"
+=======
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+>>>>>>> Stashed changes
     },
     migrations: {
         directory: "./migrations"
