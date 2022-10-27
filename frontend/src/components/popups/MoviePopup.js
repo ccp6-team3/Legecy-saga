db87eb9466466f8df2ef1619f4cb41db7bdcf93f
Brian Walvoord <brian.walvoord@gmail.com>
2022-10-27 21:42:47 +0900

    feat: in-process of adding reviews

--- frontend/src/components/popups/MoviePopup.js
+++ frontend/src/components/popups/MoviePopup.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../styles/Popup.css";
+import { useState, useEffect } from "react";
 import Card from 'react-bootstrap/Card';
 import CloseButton from 'react-bootstrap/CloseButton';
 import Reviews from './Reviews.js';
@@ -16,6 +17,25 @@ const MoviePopup = (props) => {
     setMoviePopup(false)
   }
 
+  // const [reviews, setReviews] = useState([])
+  // const getReviews = (movieID) =>{
+  //   fetch("/reviewsMovie", {
+  //     headers: {
+  //       'movieID': movieID
+  //     }
+  //   })
+  //   .then((data) => data.json())
+  //   .then((arr) => {  
+  //     setReviews(arr)
+  //   })
+  // }
+  // useEffect(() => {
+  //   getReviews(movieID)
+  // },[movieID])
+
+
+  
+
   return (
     <Modal show={true} onHide={closePopup}>
       <Card className="popup-container">
@@ -27,7 +47,7 @@ const MoviePopup = (props) => {
           <h3 className="description">Release: {selection.releaseDate}</h3>
           <p className="description">{selection.movieDescription ? selection.movieDescription : <em>*No description*</em>}</p>
         </Card.Body>
-        <Reviews />
+        <Reviews selection={selection} />
       </Card>
     </Modal>
     // <div className="popup-page">
--- frontend/src/components/popups/Reviews.js
+++ frontend/src/components/popups/Reviews.js
@@ -1,13 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../styles/Popup.css";
+import { useState, useEffect } from "react";
+
 
 const Reviews = (props) => {
-  const {} = props;
+  const {selection} = props;
+
+  // const [reviews, setReviews] = useState([])
+
+  // useEffect(()=> {
+  //   setMovieID(selection.movieID)
+  //   fetch("/reviewsMovie", {
+  //     headers: {
+  //       'movieID': selection.movieID
+  //     }
+  //   })
+  //   .then((data) => data.json())
+  //   .then((arr) => {  
+  //     setReviews(arr)
+  //   })
+  // })
+
+  // const headers = {
+  //   'movieID': selection.movieID
+  // }
+
+  // useEffect(()=> {
+  //   fetch("/reviewsMovie", headers)
+  //   .then((data) => data.json())
+  //   .then((arr) => {  
+  //     setReviews(arr)
+  //   })
+  // })
+
+
+  
 
   return (
     <>
       <h5><em>*This is the review submit form*</em></h5>
-      <h5><em>*This is the review section*</em></h5>
+      <h5><em>bla</em></h5>
     </>
   )
 }
