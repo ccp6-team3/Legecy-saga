4942e7b23269840ad0fe98140b9ae1e4983381f2
YJK-7 <2019yjk@gmail.com>
2022-10-29 11:37:57 +0900

    feat: combined filter functions and made them able to do compound searches, added a reset button to reset filters

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -16,7 +16,7 @@ const Movie = (props) => {
   const [movieSort, setMovieSort] = useState([]);
   const [movieGenres, setMovieGenres] = useState([]);
   const [movieArray, setMovieArray] = useState([]);
-  // const [compoundFilter, setCompoundFilter] = useState({});
+  const [compoundFilter, setCompoundFilter] = useState({});
   
   const genreFetch = fetch("/movieGenres");
   const ratingFetch = fetch("/movieSortBy");
@@ -25,7 +25,7 @@ const Movie = (props) => {
     Promise.all([genreFetch,ratingFetch])
     .then((promises) => {
       // returns two promises that need to be json() and passed on to next .then
-      // return promises[0].json() // makes arr into genre array
+      // return promises[0].json() // makes arr into genre array?
       return Promise.all(promises.map(dataArr => dataArr.json()))
   
     })
@@ -54,66 +54,96 @@ const Movie = (props) => {
   //   })
   // },[])
 
+
   // combined search function that compounds searches 
-  // const updateCompound = (input) => {
-  //   setCompoundFilter({...compoundFilter, ...input});
+
+  const updateCompound = (input) => {
+    setCompoundFilter({...compoundFilter, ...input});
+  }
+
+  //{genre: 18, rating:6, sort_by:yay}
+
+  useEffect(() => {
+    //{type: id/num}
+    console.log(compoundFilter)
+    if (Object.keys(compoundFilter).length !== 0) {
+      fetch("/searchMovies", {
+        headers: compoundFilter
+      })
+      .then((data) => data.json())
+      .then((arr) => {  
+        console.log(arr)
+        setMovieArray(arr);
+      })
+    }
+  },[compoundFilter])
+
+  // const filterByGenre = (genre) => {
+  //   fetch("/searchMovies", {
+  //     headers: {
+  //       'genre': genre
+  //     }
+  //   })
+  //   .then((data) => data.json())
+  //   .then((arr) => {  
+  //     // setMovieArray(arr.slice(0,10))
+  //     setMovieArray(arr)
+  //   })
   // }
 
-  // useEffect(() => {
-  //   //{type: id/num}
-  //   console.log(compoundFilter)
-  //   if (Object.keys(compoundFilter).length !== 0) {
-  //     fetch("/searchMovies", {
-  //       headers: compoundFilter
-  //     })
-  //     .then((data) => data.json())
-  //     .then((arr) => {  
-  //       console.log(arr)
-  //       setMovieArray(arr.slice(0,4));
-  //     })
-  //   }
-  // },[compoundFilter])
+  // const filterByRating = (rating) => {
+  //   fetch("/searchMovies", {
+  //     headers: {
+  //       'rating': rating
+  //     }
+  //   })
+  //   .then((data) => data.json())
+  //   .then((arr) => {  
+  //     // setMovieArray(arr.slice(0,10))
+  //     setMovieArray(arr)
+  //   })
+  // }
 
-  const filterByGenre = (genre) => {
-    fetch("/searchMovies", {
-      headers: {
-        'genre': genre
-      }
-    })
-    .then((data) => data.json())
-    .then((arr) => {  
-      // setMovieArray(arr.slice(0,10))
-      setMovieArray(arr)
-    })
-  }
+  // const filterByOther = (otherField) => {
+  //   fetch("/searchMovies", {
+  //     headers: {
+  //       'sort_by': otherField
+  //     }
+  //   })
+  //   .then((data) => data.json())
+  //   .then((arr) => {  
+  //     // setMovieArray(arr.slice(0,10))
+  //     setMovieArray(arr)
+  //   })
+  // }
 
-  const filterByRating = (rating) => {
-    fetch("/searchMovies", {
-      headers: {
-        'rating': rating
-      }
-    })
-    .then((data) => data.json())
-    .then((arr) => {  
-      // setMovieArray(arr.slice(0,10))
-      setMovieArray(arr)
-    })
-  }
+  // async function fuckOff() {
+  //   const typesOfFuck = ["asshole","because","bucket","bye"];
+  //   const type = typesOfFuck[Math.floor(Math.random() * typesOfFuck.length)];
+  //   const from = "Saga Team ;)"
 
-  const filterByOther = (otherField) => {
-    fetch("/searchMovies", {
-      headers: {
-        'sort_by': otherField
-      }
-    })
-    .then((data) => data.json())
-    .then((arr) => {  
-      // setMovieArray(arr.slice(0,10))
-      setMovieArray(arr)
-    })
+  //   const result = await fetch(`https://foaas.com/${type}/${from}`,{
+  //     headers: {
+  //       'Accept': 'application/json'
+  //     }
+  //   })
+  //   const jsResult = await result.json();
+  //   return jsResult;
+  // }
+  const resetShow = () => {
+    setCompoundFilter({});
+    setMovieArray([]);
   }
 
   const movieCards = (arrayEl) => {
+  //   let fuckOffMessage;
+    
+  //   fuckOff().then((data) => {
+  //     fuckOffMessage = data.message + data.subtitle;
+  //     console.log(fuckOffMessage)
+  //     return fuckOffMessage;
+  //   })
+
     return (
       <Card key={arrayEl.movieID} className="movieCard">
         <Card.Img 
@@ -138,45 +168,45 @@ const Movie = (props) => {
 
   
 
+  // const mapGenresArr = (arr) => {
+  //   return (
+  //     <NavDropdown.Item /*href={`#${arr.name}`}*/ onClick = {() => filterByGenre(arr.id)}>{arr.name}</NavDropdown.Item>
+  //   )
+  // }
+  
+  // const mapMovieRate = (i) => {
+  //   return (
+  //     <NavDropdown.Item /*href={`#${i}`}*/ onClick = {() => filterByRating(i)}>{i} and up</NavDropdown.Item>
+  //     )
+  // }
+
+  // const mapOtherArr = (arr) => {
+  //   return (
+  //     <NavDropdown.Item /*href={`#${arr}`}*/ onClick = {() => filterByOther(arr)}>{arr}</NavDropdown.Item>
+  //   )
+  // }
+
+  // using useEffect and compound
+  let movieRateArr = [0,1,2,3,4,5,6,7,8,9];
+
+
   const mapGenresArr = (arr) => {
     return (
-      <NavDropdown.Item /*href={`#${arr.name}`}*/ onClick = {() => filterByGenre(arr.id)}>{arr.name}</NavDropdown.Item>
+      <NavDropdown.Item /*href={`#${arr.name}`}*/ onClick = {() => updateCompound({"genre":arr.id})} >{arr.name}</NavDropdown.Item>
     )
   }
-  
-  let movieRateArr = [0,1,2,3,4,5,6,7,8,9];
 
   const mapMovieRate = (i) => {
     return (
-      <NavDropdown.Item /*href={`#${i}`}*/ onClick = {() => filterByRating(i)}>{i} and up</NavDropdown.Item>
+      <NavDropdown.Item /*href={`#${i}`}*/ onClick = {() => updateCompound({"rating":i})}>{i} and up</NavDropdown.Item>
       )
   }
 
   const mapOtherArr = (arr) => {
     return (
-      <NavDropdown.Item /*href={`#${arr}`}*/ onClick = {() => filterByOther(arr)}>{arr}</NavDropdown.Item>
+      <NavDropdown.Item /*href={`#${arr}`}*/ onClick = {() => updateCompound({"sort_by":arr})}>{arr}</NavDropdown.Item>
     )
   }
-
-  // using useEffect and compound
-
-  // const mapGenresArr2 = (arr) => {
-  //   return (
-  //     <NavDropdown.Item /*href={`#${arr.name}`}*/ onClick = {() => updateCompound({"genre":arr.id})} >{arr.name}</NavDropdown.Item>
-  //   )
-  // }
-
-  // const mapMovieRate2 = (i) => {
-  //   return (
-  //     <NavDropdown.Item /*href={`#${i}`}*/ onClick = {() => updateCompound({"rating":i})}>{i} and up</NavDropdown.Item>
-  //     )
-  // }
-
-  // const mapOtherArr2 = (arr) => {
-  //   return (
-  //     <NavDropdown.Item /*href={`#${arr}`}*/ onClick = {() => updateCompound({"sort_by":arr})}>{arr}</NavDropdown.Item>
-  //   )
-  // }
 
   return(
   <>
@@ -205,22 +235,10 @@ const Movie = (props) => {
               {movieSort.map(mapOtherArr)}
             </NavDropdown>
 
-            {/* testout */}
-            {/* <NavDropdown title="Genre2" id="navbarScrollingDropdown">
-              {movieGenres.map(mapGenresArr2)}
-            </NavDropdown>
-
-            <NavDropdown title="Rating2" id="navbarScrollingDropdown">
-              {movieRateArr.map(mapMovieRate2)}
-            </NavDropdown>
-
-            <NavDropdown title="Other2" id="navbarScrollingDropdown">
-              {movieSort.map(mapOtherArr2)}
-            </NavDropdown> */}
           </Nav>
 
           <Nav>
-            <Button className="d-grid gap-2" variant="outline-dark" /*onClick={() => setCompoundFilter({})}*/>Reset Filters</Button>
+            <Button className="d-grid gap-2" variant="outline-dark" onClick={resetShow}>Reset Filters</Button>
           </Nav>
           
         </Navbar.Collapse>
